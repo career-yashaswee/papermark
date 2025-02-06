f2040e149d6aa26e8183452d3f1bebc0e5ef7817
career-yashaswee <career.yashaswee@gmail.com>
2025-02-06 16:15:00 +0530

    feat(config): editor and app config

---
+++ src/config/index.ts
@@ -0,0 +1,57 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Papermark",
+  description: "Full-stack Realtime Docs using Next.js 15 and Convex.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "convex",
+    "convex-db",
+    "clerk-auth",
+    "tiptap-editor",
+    "lucide-icons",
+    "react-icons",
+    "livekit-io",
+    "shadcn-ui",
+    "radix-ui",
+    "tailwindcss",
+    "lodash",
+    "react-query",
+    "nuqs",
+    "sonner",
+
+    "typescript",
+    "javascript",
+    "vercel",
+    "postcss",
+    "prettier",
+    "eslint",
+    "react-dom",
+    "html",
+    "css",
+
+    "nuqs-state-management",
+    "infinite-query",
+    "real-time-documents",
+    "organized-editor",
+    "realtime-editor",
+    "paginate-documents",
+    "documents-management",
+    "ui/ux",
+    "papermark",
+
+    "date-fns",
+    "cn",
+    "clsx",
+    "lucide-react",
+  ] as Array<string>,
+  authors: {
+    name: "Yashaswee Kesharwani",
+    url: "https://github.com/career-yashaswee",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/career-yashaswee/papermark",
+} as const;
